2542c0d3be81fa06d4abace850438d0a9ec785d6
Magdalena Lanza <magdalan1993@gmail.com>
2023-03-06 18:06:24 -0300

    guardar

--- Agenda/models.py
+++ Agenda/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Concierto(models.Model):
@@ -44,3 +44,6 @@ class VidaNocturna(models.Model):
         verbose_name_plural = 'Vidas Nocturnas'
     
 class Avatar(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank=True, default=)
